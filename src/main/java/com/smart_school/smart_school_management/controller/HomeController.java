3658fccde0a0ccd5c7154d1553e1cf7732c2341e
lahumisal <lahumisal402@gmail.com>
2025-11-29 20:05:41 +0530

    reset structure

--- src/main/java/com/smart_school/smart_school_management/controller/HomeController.java
+++ src/main/java/com/smart_school/smart_school_management/controller/HomeController.java
@@ -1,48 +1,12 @@
 package com.smart_school.smart_school_management.controller;
 
-import com.smart_school.smart_school_management.model.request.LoginRequest;
-import com.smart_school.smart_school_management.model.response.LoginResponse;
-import com.smart_school.smart_school_management.service.UserService;
-import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/home")
 public class HomeController {
-
-    @Autowired
-    private UserService userService;
-
-    @GetMapping
-    public String home() {
-        return "Home";
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity registerUser(@Valid @RequestBody RegisterUser registerUser){
-        return null;
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-        log.info("Login Request: {}", loginRequest);
-        LoginResponse response=userService.loginUser(loginRequest);
-        if (response != null) {
-            return ResponseEntity.ok(response);
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    @PostMapping("/logout")
-    public String logout() {
-        return "Logout";
-    }
-
 
 
 }
--- src/main/java/com/smart_school/smart_school_management/model/enums/Roles.java
+++
@@ -1,9 +0,0 @@
-package com.smart_school.smart_school_management.model.enums;
-
-
-public enum Roles {
-
-    Principal,
-    Teacher,
-    Student,
-}
