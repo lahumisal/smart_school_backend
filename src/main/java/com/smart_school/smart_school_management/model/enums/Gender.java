4305e241f447d357309a5d4d3410daf9811f43a0
lahumisal <lahumisal402@gmail.com>
2025-11-30 22:21:20 +0530

    add classes

--- src/main/java/com/smart_school/smart_school_management/model/enums/Gender.java
+++ src/main/java/com/smart_school/smart_school_management/model/enums/Gender.java
@@ -3,5 +3,6 @@ package com.smart_school.smart_school_management.model.enums;
 public enum Gender {
     MALE,
     FEMALE,
+    TRANSGENDER,
     OTHER
 }
---
+++ src/main/java/com/smart_school/smart_school_management/model/enums/Roles.java
@@ -0,0 +1,7 @@
+package com.smart_school.smart_school_management.model.enums;
+
+public enum Roles {
+    PRINCIPAL,
+    TEACHER,
+    STUDENT
+}
