cb068749a78b8f759795493b8434c3ea4f9d31f8
lahumisal <lahumisal402@gmail.com>
2025-12-06 14:28:04 +0530

    add exception handling

--- src/main/java/com/smart_school/smart_school_management/exceptions/ExceptionTypes/MobileNumberException.java
+++ src/main/java/com/smart_school/smart_school_management/exceptions/ExceptionTypes/MobileNumberException.java
@@ -1,7 +1,18 @@
 package com.smart_school.smart_school_management.exceptions.ExceptionTypes;
 
+import com.smart_school.smart_school_management.exceptions.ExceptioonEnums.ValidationErrors;
+
 public class MobileNumberException extends RuntimeException{
-    public MobileNumberException(String s){
-        super(s);
+
+    private final ValidationErrors error;
+
+    public MobileNumberException(ValidationErrors error) {
+        super(error.getErrorMessage());
+        this.error = error;
     }
+
+    public ValidationErrors getError() {
+        return error;
+    }
+
 }
