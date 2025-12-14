43624695116b19fc26b6200eeb91b69db3e351c1
lahumisal <lahumisal402@gmail.com>
2025-12-14 11:53:34 +0530

    add field message error

--- src/main/java/com/smart_school/smart_school_management/exceptions/ExceptioonEnums/FieldErrorMessages.java
+++ src/main/java/com/smart_school/smart_school_management/exceptions/ExceptioonEnums/FieldErrorMessages.java
@@ -2,4 +2,18 @@ package com.smart_school.smart_school_management.exceptions.ExceptioonEnums;
 
 public class FieldErrorMessages {
     public static final String MOBILE_FIELD_MISSING = "MOBILE NUMBER MISSING";
+    public static final String INVALID_EMAIL_FORMAT = "INVALID EMAIL FORMAT";
+    public static final String INVALID_MOBILE_FORMAT = "INVALID MOBILE FORMAT";
+    public static final String INVALID_PASSWORD_FORMAT = "INVALID PASSWORD FORMAT";
+    public static final String INVALID_GENDER_FORMAT = "INVALID GENDER FORMAT";
+    public static final String FIRST_NAME_NOT_BLANK = "FIRST NAME NOT BLANK";
+    public static final String LAST_NAME_NOT_BLANK = "LAST NAME NOT BLANK";
+    public static final String EMAIL_NOT_BLANK = "EMAIL NOT BLANK";
+    public static final String PASSWORD_NOT_BLANK = "PASSWORD NOT BLANK";
+    public static final String GENDER_NOT_BLANK = "GENDER NOT BLANK";
+    public static final String PROFILE_PICTURE_NOT_BLANK = "PROFILE PICTURE NOT BLANK";
+    public static final String DEPARTMENT_NOT_BLANK = "DEPARTMENT NOT BLANK";
+    public static final String SUBJECT_NOT_BLANK = "SUBJECT NOT BLANK";
+    public static final String ADDRESS_NOT_BLANK = "ADDRESS NOT BLANK";
+
 }
